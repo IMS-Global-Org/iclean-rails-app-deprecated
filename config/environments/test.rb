ac2ce6495599152eb8c3467a4869b18ee54c55b5
IMS-Global <brennick.sci@gmail.com>
2019-04-29 21:24:24 -0600

    Gems added
    - faker
    - letter_opener
    - access_granted
    - rspec
    - pry-byebug

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -1,5 +1,6 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
+  config.autoload_paths += %W(#{config.root}/app/policies #{config.root}/app/roles)
 
   # The test environment is used exclusively to run your application's
   # test suite. You never need to work with it otherwise. Remember that
