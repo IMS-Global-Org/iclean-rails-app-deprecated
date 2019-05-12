3be86ce42d612cb71df7ab802b691ac5414f9d34
IMS-Global <brennick.sci@gmail.com>
2019-05-12 06:02:48 -0600

    Remove the form variable name case swapping

--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_10_225517) do
+ActiveRecord::Schema.define(version: 2019_05_10_224024) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "addresses", force: :cascade do |t|
-    t.string "street_1"
-    t.string "street_2"
+    t.string "street1"
+    t.string "street2"
     t.string "city"
     t.string "state"
     t.integer "zipcode"
