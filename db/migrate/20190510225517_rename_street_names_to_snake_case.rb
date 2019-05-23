46b77b3967e66a213eb5406c323864578d191940
IMS-Global <brennick.sci@gmail.com>
2019-05-23 03:32:24 -0600

    Removed old unused migration
    - delete migration for swapping out the snakecase parameter variable
    names.

--- db/migrate/20190510225517_rename_street_names_to_snake_case.rb
+++
@@ -1,6 +0,0 @@
-class RenameStreetNamesToSnakeCase < ActiveRecord::Migration[5.2]
-  def change
-    rename_column :addresses, :street1, :street_1
-    rename_column :addresses, :street2, :street_2
-  end
-end
